e6dc32862164a2e09bfb26d1a72ed32a12003b5d
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-04-20 19:37:50 -0600

    Rewrote the header to match footer sizes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,42 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
 
+//  max-w-7xl to constraint the header
 export default function Header2() {
   return (
     <>
       <header className="bg-amber-200">
         <nav
-          className="mx-auto flex max-w-7xl items-center p-6 lg:px-8"
+          className="mx-auto flex w-full h-12 justify-between items-center px-4 md:px-6"
           aria-label="Global"
         >
-          <div className="flex justify-start flex-1">
-            <Link href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <Image
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                width={40}
-                height={40}
-                alt="Logo"
-              />
-            </Link>
-            <Link href="/extra" className="-m-1.5 p-1.5">
-              Extra Link
+          <div className="flex md:flex-1 justify-start ">
+            <Link href="/extra" className="text-3xl font-bold">
+            严
             </Link>
           </div>
 
-          <div className="flex justify-center">
-            <h2 className="font-bold">Yipee</h2>
-          </div>
+          {/* <div className="flex justify-center">
+            <h2 className="font-bold">-------- SEARCH BAR HERE --------</h2>
+          </div> */}
 
-          <div className="flex justify-end flex-1 gap-x-12">
-            <Link href="/first" className="-m-1.5 p-1.5">
-              Click on Here
+          <div className="flex md:flex-1 justify-end items-center gap-x-2 md:gap-x-6">
+            <Link href="/blog" className="underline">
+              BLOG
             </Link>
-            <Link href="/second" className="-m-1.5 p-1.5">
-              But not here
+            <Link href="/projects" className="underline">
+              PROJECTS
             </Link>
-            <Link href="/third" className="-m-1.5 p-1.5">
-              Third Link
+            <Link href="/about-me" className="underline">
+              ABOUT ME
             </Link>
           </div>
         </nav>

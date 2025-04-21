6f685d4c13afff06916bae4ef4abba6bf3f78a7c
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-04-20 19:23:07 -0600

    Rewrote the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,28 +1,45 @@
-// Shitty figma code export and AI. Need to 
+import { IconContext } from "react-icons";
+import { FaRss, FaGithub, FaLinkedin, FaCopyright } from "react-icons/fa";
+import { MdOutlineLightMode, MdOutlineDarkMode, MdMail } from "react-icons/md"; // TODO Add light/dark switch with next-theme
+
 export default function Footer() {
-    return (
-      <footer className="w-full h-12 relative overflow-hidden bg-green-500 flex justify-between items-center px-6">
-        <div className="flex items-center gap-x-6">
-          <div className="text-black text-sm font-semibold font-['Inter'] leading-none">
-            RSS FEED
-          </div>
-          <img className="w-7 h-7" src="https://placehold.co/30x30" />
-          <div className="text-black text-sm font-semibold font-['Inter'] leading-none">
-            THEME
-          </div>
-        </div>
-        <div className="text-black text-sm font-semibold font-['Inter'] leading-none opacity-80">
-          2025 Ryan Yan
-        </div>
-        <div className="flex items-center gap-x-6">
-          <div className="w-7 h-7 bg-black" />
-          <div className="w-7 h-7 bg-black" />
-          <div className="w-11 h-11">
-            <div className="w-7 h-6 rounded-sm outline outline-1 outline-offset-[-0.50px] outline-neutral-800 relative top-3" />
-            <div className="w-7 h-2 rounded-sm border border-neutral-800 relative top-3" />
-          </div>
-        </div>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer
+      className="mx-auto w-full h-12 bg-green-500 flex justify-between items-center px-4 md:px-6"
+      aria-label="Global"
+    >
+      <div className="flex flex-1 justify-start items-center gap-x-2 md:gap-x-3">
+        <p className="text-black text-sm font-semibold leading-none visible md:hidden">RSS</p>
+        <p className="text-black text-sm font-semibold leading-none hidden md:block">RSS FEED</p>
+        <FaRss />
+        <div className="w-0.5 md:w-1.5"></div>
+        <p className="text-black text-sm font-semibold leading-none visible md:hidden">T</p>
+        <p className="text-black text-sm font-semibold leading-none hidden md:block">THEME</p>
+        <MdOutlineLightMode size="1.5em" />
+      </div>
+
+      <div className="flex justify-center items-center gap-x-1 text-black text-sm font-semibold">
+        <FaCopyright size="0.6em" />
+        2025 Ryan Yan
+      </div>
+
+      <div className="flex flex-1 justify-end items-center gap-x-2 md:gap-x-5">
+        <a href="https://github.com/Madrobot9182" target="_blank">
+          <FaGithub size="1.5em" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ryan-yan-b296a4205/"
+          target="_blank"
+        >
+          <FaLinkedin size="1.5em" />
+        </a>
+        <a
+          href="mailto:madrobot9182@protonmail.com?subject=Getting%20in%20Contact&body=Hello,%20I'm%20interested%20in%20learning%20more.%20Please%20contact%20me:%20"
+          target="_blank"
+        >
+          <MdMail size="1.5em" />
+        </a>
+      </div>
+    </footer>
+  );
+}

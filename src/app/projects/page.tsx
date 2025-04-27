9f71d96016dc061a766571b70992511a6226b070
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-04-27 10:47:28 -0600

    Formatted the projects card

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,29 +7,28 @@ export default function ProjectGrid() {
 
   return (
     <div className="mx-auto my-10 max-w-7xl px-6">
-      <h1 className="text-5xl font-bold text-center md:text-left">Projects</h1>
-      <div className="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 md:ps-8">
+      <h1 className="text-5xl font-bold text-center mb-6 md:text-left">
+        Projects
+      </h1>
+      <div className="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:ps-8">
         {projects.map((project) => (
-          <div key={project.slug} className="border-b h-100 bg-red-100">
-            <Link
-              href={`/projects/${project.slug}`}
-              className="hover:underline"
-            >
-              <h2 className="text-xl font-medium">
-                {project.frontMatter.title}
-              </h2>
-              <Image
+          <Link
+            href={`/projects/${project.slug}`}
+            className="hover:underline p-8 rounded-xl border-b-2 border-b-gray-300"
+            key={project.slug}
+          >
+            <h2 className="text-xl font-medium">{project.frontMatter.title}</h2>
+            <p className="text-gray-500">
+                {project.frontMatter.date}
+              </p>
+            {/* <Image
                 src={project.imagePath}
                 width={300}
                 height={200}
                 alt={project.slug + " cover image"}
-              />
-              <p className="line-clamp-2 text-gray-600">
-                {" "}
-                {getExcerpt(project.content)}
-              </p>
-            </Link>
-          </div>
+              /> */}
+            <p className="line-clamp-4">{getExcerpt(project.content)}</p>
+          </Link>
         ))}
       </div>
     </div>

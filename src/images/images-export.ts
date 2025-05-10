dc44fc3016c526a61cb99cc3b124a0ec4fdc9d25
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-05-10 17:17:43 -0600

    neural-gui cover photo

--- src/images/images-export.ts
+++ src/images/images-export.ts
@@ -1,9 +1,14 @@
 // Yes I manaully import all cover photos cause I'm/Nextjs is a hack
 import GameHubCover from "../../projects/game-hub/cover.avif"
+import NeuralGUI from "../../projects/neural-gui/cover.avif"
 
 export const projectCovers = [
     {
         image: GameHubCover,
         slug: "game-hub",
+    },
+    {
+        image: NeuralGUI,
+        slug:"neural-gui"
     },
 ]
